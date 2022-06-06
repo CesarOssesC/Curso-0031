c62c2ce225d365f079f287f211433fde73fc9c2f
César Osses <cesar.osses@gmail.com>
2022-06-06 09:35:13 -0400

    emprendedor1

---
+++ rentabilidad/emprendedor1.rb
@@ -0,0 +1,5 @@
+puts "-----------------------------------------------"
+puts "|Bienvenido al sistema de cálculo de utilidad1|"
+puts "-----------------------------------------------"
+print "Ingresa el precio de producto"
+precio = gets.chomp.to_i
