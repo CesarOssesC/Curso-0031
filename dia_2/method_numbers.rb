2cf5957e37049ed40fbdcba7e58aae8497fa50f5
César Osses <cesar.osses@gmail.com>
2022-06-06 09:30:33 -0400

    nueva carpeta

---
+++ dia_2/method_numbers.rb
@@ -0,0 +1,11 @@
+numero_entero = 1776
+puts numero_entero.next
+puts numero_entero.succ
+
+puts numero_entero.odd?
+puts numero_entero.even?
+
+puts 3**2
+
+puts numero_entero.to_s.length
+puts numero_entero.digits
